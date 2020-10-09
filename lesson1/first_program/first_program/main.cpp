13e62f9c2c079213007b9ed9b4347a784b0c42c2
julianbakerphd <julianbakerphd@gmail.com>
2020-10-09 11:11:20 -0400

    initial commit

---
+++ lesson1/first_program/first_program/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  first_program
+//
+//  Created by A to Z Coding on 10/9/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
